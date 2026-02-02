c89c01901a8e477d3ff4334ef8745c2241976db9
Youssef Al Mostafa <youssef.m@codendot.com>
2026-02-02 13:48:30 +0200

    ...

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gamma
-https://www.geeksforgeeks.org/node-js/steps-to-create-an-express-js-application/
+https://www.geeksforgeeks.org/node-js/steps-to-create-an-express-js-application/  setp 7 
+
+if the port is already in use 
+
+ taskkill /F /IM node.exe
+
+  Or in PowerShell:
+  Get-Process node | Stop-Process -Force
+
+
--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
-
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200);
-  res.send("Welcome to root URL 5 of Server");
-});
+app.use(express.json());
+app.post('/', (req, res) => {
+  const { name } = req.body;
+
+  res.send(`Welcome ${name}`);
+})
+
 
 app.listen(PORT, (error) => {
-  if (!error)
-    console.log("Server is Successfully Running,  and App is listening on port "+ PORT);
-    else
-      console.log("Error occurred, server can't start", error);
-}
-);
+  if (!error) {
+    console.log("Server is Successfully Running, and App is listening on port " + PORT);
+  } else {
+    console.log("Error occurred, server can't start", error);
+  }
+});
